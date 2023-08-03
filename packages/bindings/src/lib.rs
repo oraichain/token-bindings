5299b86bcdb898c0dc4f194b1c910153d1cfbddc
Reece Williams <reecepbcups@gmail.com>
2023-08-03 07:50:48 -0500

    0.10.3 - removes required_tf function call

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -10,8 +10,3 @@ pub use query::{
     TokenFactoryQuery, TokenQuery,
 };
 pub use types::{DenomUnit, Metadata, Params};
-
-// This is a signal, such that any contract that imports these helpers will only run on
-// blockchains that support token_factory feature
-#[no_mangle]
-extern "C" fn requires_token_factory() {}
