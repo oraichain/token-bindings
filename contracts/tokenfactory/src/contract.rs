39fd33db9f8c55cb0314c39aa963013eee2e610a
trung2891 <trungnv280201@gmail.com>
2025-01-16 17:04:18 +0700

    chore:dont set version

--- contracts/tokenfactory/src/contract.rs
+++ contracts/tokenfactory/src/contract.rs
@@ -416,7 +416,6 @@ fn validate_denom_owner(
 pub fn migrate(deps: DepsMut, _env: Env, msg: MigrateMsg) -> Result<Response, TokenFactoryError> {
     let config = Config { owner: msg.owner };
 
-    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     CONFIG.save(deps.storage, &config)?;
     Ok(Response::default())
 }
